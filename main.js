bd9a444fcfbebf5c040c97842c4d95d0c3e204ec
SachinKumar016 <jhasachinkumar2006@gmail.com>
2025-01-31 23:45:01 +0530

    fourth commit

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ const scroll= new LocomotiveScroll({
     el:document.querySelector('[data-scroll-container]'),
     smooth:true
 });
-let rating=[];
+rating=[];
+function handle(){
+    let ra=prompt("Give your rating (1-5):")
+    if(ra===null){
+        break;
+    }
+    rating.push(ra)
+}
+console.log(rating)
